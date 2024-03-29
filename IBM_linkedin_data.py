23ec9395da51c638f06b05a1a7a4d7738f689332
Garv Saxena <garvsaxena185@gmail.com>
2024-03-29 17:49:43 +0530

    Update IBM_linkedin_data.py

--- IBM_linkedin_data.py
+++ IBM_linkedin_data.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
+from datetime import datetime as dt
 import pyperclip as pc
 import regex as re
 import csv
@@ -34,19 +35,30 @@ typ = driver.find_elements(By.XPATH, """/html/body/div[4]/div[3]/div/div[2]/div/
 ##driver.find_elements(By.XPATH, """/html/body/div[4]/div[3]/div/div[2]/div/div[2]/main/div[2]/div/div[2]/div[2]/div/div/div/ul/li[2]/div/button""")[0].click()
 ##time.sleep(2)
 #driver.execute_script("window.scrollTo(0, 1800)")
-
+with open("IBM_post_data.csv","x", newline='') as file: 
+            wr = csv.writer(file)
+            wr.writerow(["postID", "tm_stamp", "typ", "fol", "react", "comments", "repost", "edit", "link", "exact_date"])
 a = 0
 b = 0
-for i in range(1,10):
+for i in range(1,5000):
     
-    print(f"----------------------------{i}th - post str-----------------------------------")
+    print(f"{i} post str")
     WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, f"""/html/body/div[4]/div[3]/div/div[2]/div/div[2]/main/div[2]/div/div[2]/div[2]/div/div[1]/div[{i}]/div/div/div/div/div""")))
     text = driver.find_elements(By.XPATH, f"""/html/body/div[4]/div[3]/div/div[2]/div/div[2]/main/div[2]/div/div[2]/div[2]/div/div[1]/div[{i}]/div/div/div/div/div""")[0].text
     #print(text)
     fol = int(re.findall('\d(?:\d|,|)+\d followers*', text)[0].split(' ')[0].replace(',',''))
+
+    try:
+        all_react = re.findall('\d(?:\d|,|)*\d\s(?:(?:\d|,|)*\d comments*\s)?(?:(?:\d|,|)*\d reposts*\s)?Like\sComment\sRepost\sSend', text)[0].split('\n')
+        react = int(all_react[0].replace(',',''))
+        
+    except:
+        all_react = re.findall('/^(?:.*?)others\s(?:(?:\d|,|)*\d comments*\s)?(?:(?:\d|,|)*\d reposts*\s)?Like\sComment\sRepost\sSend/', text)[0].split('\n')
+        react = int(all_react[0].replace(',','').split(' ')[-2])+1
+        print('other wala case')
+
     
-    all_react = re.findall('\d(?:\d|,|)*\d\s(?:(?:\d|,|)*\d comments*\s)?(?:(?:\d|,|)*\d reposts*\s)?Like\sComment\sRepost\sSend', text)[0].split('\n')
     react = int(all_react[0].replace(',',''))
     
     if len(all_react)>5 and 'comment' in all_react[1]:
@@ -80,7 +92,8 @@ for i in range(1,10):
         link = pc.paste()
         postID = re.findall('[0-9]{19}', link)
         tm_stamp = (int(postID[0]) >> len(bin(int(postID[0])))-43)//1000
-        exact_date = time.ctime(tm_stamp)
+        exact_date = dt.fromtimestamp(tm_stamp).strftime('%d-%m-%Y')
+        #exact_date = time.ctime(tm_stamp)
         size = driver.find_elements(By.XPATH, f"""/html/body/div[4]/div[3]/div/div[2]/div/div[2]/main/div[2]/div/div[2]/div[2]/div/div[1]/div[{i}]""")[0].size
         #print(size)
 
@@ -93,15 +106,20 @@ for i in range(1,10):
 
         #print("yus i executed")
         #print('fol: \n',fol,'\nreact: \n',react,'\nedit: \n',edit,'\npostID:\n',postID)
-        ind = text.rfind('•')
-        txt = text[ind:ind+30].casefold().replace('\n',' ')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+        #ind = text.rfind('•')
+        #txt = text[ind:ind+30].casefold().replace('\n',' ')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
         #print("Post ID: "+postID[0],"Type: "+typ,"followers: "+fol[0],"Reaction: "+react,"Edited : "+edit,"Link: "+link,sep='\n')
-        row = [int(postID[0]), tm_stamp, typ, fol, react, comments, repost, edit, link, txt, exact_date]
-        print(row)
+        row = [int(postID[0]), tm_stamp, typ, fol, react, comments, repost, edit, link, exact_date]
+        #["postID", "tm_stamp", "typ", "fol", "react", "comments", "repost", "edit", "link", "exact_date"]
+        #print(row)
+
+        with open("IBM_post_data.csv","a",newline='') as file: 
+            wr = csv.writer(file)
+            wr.writerow(row)
 
     except Exception as e:
-        print("not real post")
+        print(f"not real post i value = {i}")
         print(traceback.format_exc())
         print(e)
     
@@ -113,7 +131,9 @@ for i in range(1,10):
 ##        print("I also")
 
 
-    print(f"---------------------------{i}th - post end-------------------")
+    print(f"{i}post end")
+
+print("All done")
 
 
 
